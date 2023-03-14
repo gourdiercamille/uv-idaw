4d85315bcaa44b66a3c85de27c520c6f0d38e70a
gourdiercamille <camille.gourdier@etu.imt-nord-europe.fr>
2023-03-14 12:12:46 +0100

    commit2.2

--- SitePro/v2/template_menu.php
+++ SitePro/v2/template_menu.php
@@ -8,19 +8,26 @@
 </nav>
 
 <?php
-    function renderMenuToHTML($currentPageId) {
-        $mymenu = array(
-            'index' => array( 'Menu' ),
-            'cv' => array( 'CV' ),
-            'hobbies' => array( 'Hobbies' ),
-            'projets' => array('Skills')
-        );
-        foreach($mymenu as $pageId => $pageParameters) {
-            if $currentPageId==$pageId{
-                
+        function renderMenuToHTML($currentPageId) {
+            echo '<nav class="menu rouge"><ul>';
+            $mymenu = array(
+                // idPage titre
+                'index' => array('Menu'),
+                'cv' => array('Cv'),
+                'projets' => array('Skills'),
+                'hobbies' => array('Hobbies')
+            );
+        
+            foreach($mymenu as $pageId => $pageParameters) {
+                // Vérifie si la page en cours correspond à l'ID de la page en cours de la boucle.
+                if ($pageId == $currentPageId) {
+                    // Si c'est le cas, on ajoute l'identifiant "currentPage" à l'élément <a>.
+                    echo '<a id="currentPage" href="' . $pageId . '.php">' . $pageParameters[0] . '</a><br>';
+                } else {
+                    // Sinon, on génère un lien normal.
+                    echo '<a href="' . $pageId . '.php">' . $pageParameters[0] . '</a><br>';
+                }
             }
-            echo "...";
-        }
-
-    }
+            echo"</ul></nav>";
+        } 
 ?>
