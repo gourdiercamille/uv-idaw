976e13b0c16c13b142c579cb4d84c34a7929ae0a
gourdiercamille <camille.gourdier@etu.imt-nord-europe.fr>
2023-03-29 11:56:06 +0200

    modifs config

--- Projet/frontend/config.php
+++ Projet/frontend/config.php
@@ -4,5 +4,6 @@
     define('URL_API','http://localhost:8888/IDAW_PROJET/Projet/backend/api.php');
     
     //for cam
+    define('URL_API','http://localhost:3306/IDAW_PROJET/Projet/backend/api.php');
 
     //for prof
