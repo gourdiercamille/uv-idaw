b95038bedafc27cf37374793339b56a74a24923f
gourdiercamille <camille.gourdier@etu.imt-nord-europe.fr>
2023-04-04 11:13:16 +0200

    localhost benjam

--- Projet/frontend/config.php
+++ Projet/frontend/config.php
@@ -1,21 +1,10 @@
 <?php
 
     //for ben
-<<<<<<< HEAD
-    //define('URL_API','http://localhost:8889/IDAW_PROJET/Projet/backend/');
+    //define('URL_API','http://localhost:8888/IDAW_PROJET/Projet/backend/');
     
-=======
-    define('URL_API_DASHBOARD','http://localhost:8888/IDAW_PROJET/Projet/backend/api_dashboard.php');
-    define('URL_API_PROFIL','http://localhost:8888/IDAW_PROJET/Projet/backend/api_profil.php');
-
->>>>>>> 896d311ae47b575c6ceb94ab27b2d15c08c52269
     //for cam
     define('URL_API','http://localhost:3306/IDAW_PROJET/Projet/backend/');
     
     //for prof
-<<<<<<< HEAD
     //define('URL_API', '');
-=======
-    // define('URL_API_PROFIL', '');
-    // define('URL_API_DASHBOARD', '')
->>>>>>> 896d311ae47b575c6ceb94ab27b2d15c08c52269
