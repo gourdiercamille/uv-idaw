da955c0b47e7787d080536525f3000a11f4cd531
ben212001 <benjamin.puzenat@gmail.com>
2023-04-05 11:05:55 +0200

    config + dashboard

--- Projet/backend/config.php
+++ Projet/backend/config.php
@@ -1,15 +1,6 @@
 <?php
-    
-    if($_ENV['USERNAME']='ORDI_CAMILLE$ ') {
-        // param de la machine cam
-            define('_MYSQL_HOST','127.0.0.1');
-            define('_MYSQL_PORT', 3306);
-            define('_MYSQL_DBNAME','projet_idaw');
-            define('_MYSQL_USER','root');
-            define('_MYSQL_PASSWORD','');
-        }
-    
-    else if($_ENV['DOCUMENT_ROOT']= '/Applications/MAMP/htdocs') {
+
+    if($_ENV['DOCUMENT_ROOT']= '/Applications/MAMP/htdocs') {
         // param de la machine ben
             define('_MYSQL_HOST','127.0.0.1'); 
             define('_MYSQL_PORT',8889); 
@@ -17,13 +8,22 @@
             define('_MYSQL_USER','root');
             define('_MYSQL_PASSWORD','root'); 
         }
+
+    else if($_ENV['USERNAME']='ORDI_CAMILLE$ ') {
+        // param de la machine cam
+            define('_MYSQL_HOST','127.0.0.1');
+            define('_MYSQL_PORT', 3306);
+            define('_MYSQL_DBNAME','projet_idaw');
+            define('_MYSQL_USER','root');
+            define('_MYSQL_PASSWORD','');
+    }
     
     else if($_ENV['SERVER_NAME']='io') {
-        // nom de la machine prof
-        define('_MYSQL_HOST','mysql');
-        define('_MYSQL_PORT',3306);
-        define('_MYSQL_DBNAME','FABRESSE-Luc');
-        define('_MYSQL_USER','root');
-        define('_MYSQL_PASSWORD','root');
+        // param de la machine prof
+            define('_MYSQL_HOST','mysql');
+            define('_MYSQL_PORT',3306);
+            define('_MYSQL_DBNAME','FABRESSE-Luc');
+            define('_MYSQL_USER','root');
+            define('_MYSQL_PASSWORD','root');
     }
 ?>
--- Projet/frontend/config.php
+++ Projet/frontend/config.php
@@ -2,17 +2,17 @@
 
     if($_ENV['DOCUMENT_ROOT']= '/Applications/MAMP/htdocs') {
         // param de la machine ben
-        define('URLAPI','http://localhost:8888/IDAW_PROJET/Projet/backend/');
+        define('URL_API','http://localhost:8888/IDAW_PROJET/Projet/backend/');
     }
     
     else if($_ENV['USERNAME']='ORDI_CAMILLE$ ') {
         // param de la machine cam
-        define('URLAPI','http://localhost/IDAW_PROJET/Projet/backend/');
+        define('URL_API','http://localhost/IDAW_PROJET/Projet/backend/');
     }
 
     else if($_ENV['SERVER_NAME']='io') {
         // for prof
-        define('URLAPI', '');
+        define('URL_API', '');
     }
-?>
+
     
